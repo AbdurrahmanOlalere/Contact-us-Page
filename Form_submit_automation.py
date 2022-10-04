028f7420a29547c4c38336bd6645d0880b9dbb54
LosingLogic <rayman179237@gmail.com>
2022-10-04 09:01:18 -0500

    update form submit with python automation

---
+++ Form_submit_automation.py
@@ -0,0 +1,9 @@
+import ssl
+import smtplib
+
+email_from = 'msawebmanager@gmail.com'
+password = 'oztbpsrjdfkohejc'
+email_to = 'a_olalere@tamu.edu'
+
+#string for email
+email_string = "Howdy, test msg from form submit"
